7bf4dadf7d83de7b387791a389db30de615b8903
lconklin1 <lconklin@unomaha.edu>
2025-02-15 12:53:42 -0600

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -10,25 +10,19 @@ def create_gam(data):
     gam = gam.gridsearch(np.asarray(x), y)
     return gam
 
-def modelFit(gam,data_test):
+def create_modelFit(gam,data_test):
     data_test['trips'] = data_test.apply(lambda row: gam.predict([[row.month, row.day_of_week_num, row.hour_modified]])[0], axis=1)
     return data_test
 
-def main():
-    data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
-    data['day_of_week_num'] = pd.to_datetime(data['Timestamp']).dt.weekday+1
-    data['hour_modified'] = data['hour']+1
+data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
+data['day_of_week_num'] = pd.to_datetime(data['Timestamp']).dt.weekday+1
+data['hour_modified'] = data['hour']+1
 
-    data_test = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv")
-    data_test['day_of_week_num'] = pd.to_datetime(data_test['Timestamp']).dt.weekday+1
-    data_test['hour_modified'] = data_test['hour']+1
-    data_test['trips'] = 0
+data_test = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv")
+data_test['day_of_week_num'] = pd.to_datetime(data_test['Timestamp']).dt.weekday+1
+data_test['hour_modified'] = data_test['hour']+1
+data_test['trips'] = 0
 
-    gam = create_gam(data)
-    model = gam
-    data_test = modelFit(gam, data_test)
-    pred = data_test['trips'].values 
-    return True
-main()
-
-
+model = create_gam(data)
+modelFit = create_modelFit(gam, data_test)
+pred = data_test['trips'].values 
