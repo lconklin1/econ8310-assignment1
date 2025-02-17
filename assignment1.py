5cadd54f428972f058c5ddca08ca3f6e6e7b3248
lconklin1 <lconklin@unomaha.edu>
2025-02-17 12:46:47 -0600

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -3,17 +3,6 @@ import pandas as pd
 import patsy as pt
 import numpy as np
 
-def create_gam(data):
-    eqn = """trips ~ -1 + month + day_of_week_num + hour"""
-    y,x = pt.dmatrices(eqn, data=data)
-    gam = LinearGAM(s(0) + s(1) + s(2) )
-    gam = gam.gridsearch(np.asarray(x), y)
-    return gam
-
-def create_modelFit(gam,data_test):
-    data_test['trips'] = data_test.apply(lambda row: gam.predict([[row.month, row.day_of_week_num, row.hour_modified]])[0], axis=1)
-    return data_test
-
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 data['day_of_week_num'] = pd.to_datetime(data['Timestamp']).dt.weekday+1
 data['hour_modified'] = data['hour']+1
@@ -23,6 +12,12 @@ data_test['day_of_week_num'] = pd.to_datetime(data_test['Timestamp']).dt.weekday
 data_test['hour_modified'] = data_test['hour']+1
 data_test['trips'] = 0
 
-model = create_gam(data)
-modelFit = create_modelFit(model, data_test)
+eqn = """trips ~ -1 + month + day_of_week_num + hour"""
+y,x = pt.dmatrices(eqn, data=data)
+gam = LinearGAM(s(0) + s(1) + s(2) )
+gam = gam.gridsearch(np.asarray(x), y)
+model = gam
+modelFit = model.fit(data[['month','day_of_week_num','hour']], data['trips'])
+data_test['trips'] = data_test.apply(lambda row: modelFit.predict([[row.month, row.day_of_week_num, row.hour_modified]])[0], axis=1)
+
 pred = data_test['trips'].values 
